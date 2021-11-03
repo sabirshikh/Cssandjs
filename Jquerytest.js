142e57b96cdcb4e586bc374bd1c354c0beb16aea
sabirshikh <93660374+sabirshikh@users.noreply.github.com>
2021-11-03 21:05:23 +0530

    Add files via upload

---
+++ Jquerytest.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+
+                 
+
+                 $('.code-box-copy').codeBoxCopy({
+                     tooltipText: 'Copied',
+                     tooltipShowTime: 1000,
+                     tooltipFadeInTime: 300,
+                     tooltipFadeOutTime: 300
+                 });
+        </script>
